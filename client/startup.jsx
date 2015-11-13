fa848262db7299e16d6e35faab65ee7efb221344
Henry Jin <henry@entrancesoftware.com>
2015-11-13 14:41:23 -0600

    Added react router and initial route.

--- client/startup.jsx
+++ client/startup.jsx
@@ -1,3 +1,22 @@
+const {
+  Router,
+  Route,
+  IndexRoute
+} = ReactRouter;
+
+const createHistory = ReactRouter.history.createHistory;
+
+const routes = (
+  <Route path="/" component={App}>
+  </Route>
+);
+
+const router = (
+  <Router history={createHistory()}>
+    {routes}
+  </Router>
+);
+
 Meteor.startup(function () {
-  ReactDOM.render(<App />, document.getElementById("app"));
+  ReactDOM.render(router, document.getElementById("app"));
 });
